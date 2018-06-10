44c3059d0dbb138edc89fee0a9445204b8198f2d
PrajwalM2212 <prajwalmmath@gmail.com>
2018-06-10 23:02:32 +0530

    dy

--- src/DynamicProgramming/Reverse.java
+++ src/DynamicProgramming/Reverse.java
@@ -1,4 +1,39 @@
 package DynamicProgramming;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reverse {
+
+    private ArrayList<Integer> arrayList = new ArrayList<>();
+
+    private void dynamic(int k, String n) {
+
+        for (int i = 0; i <= 9; i++) {
+            arrayList.add(k, i);
+            if (k < n.length() - 1) {
+                dynamic(k + 1, n);
+            }
+            if (k == n.length() - 1) {
+                StringBuilder sb = new StringBuilder();
+                for (int e : arrayList) {
+                    sb.append(e);
+                }
+
+                if (n.equals(sb.reverse().toString())) {
+                    System.out.println(sb.reverse().toString());
+                }
+
+            }
+            arrayList.remove(k);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.next();
+        Reverse reverse = new Reverse();
+        reverse.dynamic(0, n);
+        scanner.close();
+    }
 }
