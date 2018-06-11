df932b808a579e69f7876910f38925a62106b035
PrajwalM2212 <prajwalmmath@gmail.com>
2018-06-11 18:31:37 +0530

    dynamic programming

--- src/DynamicProgramming/MaxSum.java
+++ src/DynamicProgramming/MaxSum.java
@@ -5,46 +5,46 @@ import java.util.Scanner;
 public class MaxSum {
 
     private int m, n;
-    private int[][][] cache = new int[100][100][100];
-    private int count = 0;
+    private int[][] cache;
+    private int count;
 
     private int solve(int[][] path) {
 
         int q = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
 
-            q = Math.max(q, recur(path, path[0][i], 0, i));
+            q = Math.max(q, recur(path, 0, i));
 
         }
-
         return q;
     }
 
-    private int recur(int[][] path, int sum, int row, int col) {
+    private int recur(int[][] path, int row, int col) {
 
-       /* int a = sum + path[row + 1][col];
-        int b = sum + path[row + 1][col + 1];
-        int c = sum + path[row + 1][col - 1];*/
-
-        if (cache[sum][row][col] != 0) {
-            return cache[sum][row][col];
+        if (cache[row][col] != 0) {
+            return cache[row][col];
         }
+
         count++;
-        if (row < m - 1) {
-            int p = recur(path, sum + path[row + 1][col], row + 1, col);
-            int q = 0, r = 0;
-            if (col + 1 <= n - 1) {
-                q = recur(path, sum + path[row + 1][col + 1], row + 1, col + 1);
-            }
-            if (col - 1 >= 0) {
-                r = recur(path, sum + path[row + 1][col - 1], row + 1, col - 1);
-            }
-
-            return cache[sum][row][col] = Math.max(Math.max(p, q), r);
-
+        if (row == m - 1) {
+            return path[row][col];
         }
 
-        return cache[sum][row][col] = Math.max(Integer.MIN_VALUE, sum);
+        int p = recur(path, row + 1, col);
+        int q = 0, r = 0;
+        if (col + 1 <= n - 1) {
+            q = recur(path, row + 1, col + 1);
+        }
+        if (col - 1 >= 0) {
+            r = recur(path, row + 1, col - 1);
+        }
+
+        int sum = path[row][col];
+
+        if (cache[row][col] == 0) {
+            cache[row][col] = Math.max(Math.max(sum + p, sum + q), sum + r);
+        }
+        return cache[row][col];
 
     }
 
@@ -60,9 +60,11 @@ public class MaxSum {
                     path[i][j] = scanner.nextInt();
                 }
             }
+            int[][] cache = new int[1000][1000];
             MaxSum maxSum = new MaxSum();
             maxSum.m = m;
             maxSum.n = n;
+            maxSum.cache = cache;
             System.out.println(maxSum.solve(path));
             System.out.println(maxSum.count);
             t--;
