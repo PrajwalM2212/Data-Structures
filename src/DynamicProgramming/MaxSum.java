4a2dfa6d807ceafe5c9e1c6c067652cacd69a674
PrajwalM2212 <prajwalmmath@gmail.com>
2018-06-11 21:08:06 +0530

    full sol

--- src/DynamicProgramming/MaxSum.java
+++ src/DynamicProgramming/MaxSum.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 
 public class MaxSum {
 
+    // example array of 6 rows and 5 columns
+    /*      6 5
+            3 1 7 4 2
+            2 1 3 1 1
+            1 2 2 1 8
+            2 2 1 5 3
+            2 1 4 4 4
+            5 2 7 5 1 */
+
+
+    // index of array
     private int m, n;
+    //array to store values of sub problems
     private int[][] cache;
+    // variable to calculate number of recursion calls
     private int count;
 
+    // I loop through first row and call recur() to find the largest sum
     private int solve(int[][] path) {
 
         int q = Integer.MIN_VALUE;
@@ -19,6 +33,8 @@ public class MaxSum {
         return q;
     }
 
+    // actual recursive function
+    // rn = max{(path[row+1][col] + rn-1),(path[row+1][col+1] + rn-1),(path[row+1][col-1] + rn-1)} is the recursion relation
     private int recur(int[][] path, int row, int col) {
 
         if (cache[row][col] != 0) {
@@ -33,15 +49,18 @@ public class MaxSum {
         int p = recur(path, row + 1, col);
         int q = 0, r = 0;
         if (col + 1 <= n - 1) {
+            // if it does exceed the number of columns
             q = recur(path, row + 1, col + 1);
         }
         if (col - 1 >= 0) {
+            // if it does not exceed the number of rows
             r = recur(path, row + 1, col - 1);
         }
 
         int sum = path[row][col];
 
         if (cache[row][col] == 0) {
+            //implementation of recursive formula
             cache[row][col] = Math.max(Math.max(sum + p, sum + q), sum + r);
         }
         return cache[row][col];
