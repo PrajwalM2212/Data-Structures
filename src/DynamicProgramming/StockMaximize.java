c33ed2b164465a056c4f82118224cac2c57b6425
PrajwalM2212 <prajwalmmath@gmail.com>
2018-06-19 21:38:52 +0530

    dp

---
+++ src/DynamicProgramming/StockMaximize.java
@@ -0,0 +1,53 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+public class StockMaximize {
+
+
+    private long[] cache ;
+    private long solve(int start, long spent, int[] price, int shareCount) {
+
+        if(cache[start]!=0){
+            return cache[start];
+        }
+
+        if (start == price.length - 1) {
+            return Math.max(0, Math.max(0, ((shareCount * price[start]) - spent)));
+        }
+
+        long profit = 0;
+        //buy
+        long buy = profit + solve(start + 1, spent + (price[start]), price, shareCount + 1);
+
+        //do nothing
+        long none = profit + solve(start + 1, spent, price, shareCount);
+
+        //sold
+        long sell = Integer.MIN_VALUE;
+        if (start > 0) {
+            sell = profit + ((shareCount * price[start]) - spent) + solve(start + 1, 0, price, 0);
+        }
+
+        return cache[start]=Math.max(Math.max(buy, none), sell);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t > 0) {
+            int n = scanner.nextInt();
+            int[] prices = new int[n];
+            for (int i = 0; i < n; i++) {
+                prices[i] = scanner.nextInt();
+            }
+            long[] cache = new long[n+1];
+            StockMaximize maximize = new StockMaximize();
+            maximize.cache=cache;
+            System.out.println(maximize.solve(0, 0, prices, 0));
+            t--;
+        }
+    }
+}
