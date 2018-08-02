e4f77f27b57b1028530bc0e2c7b99f07f8d66924
PrajwalM2212 <prajwalmmath@gmail.com>
2018-08-02 13:21:54 +0530

    Breadth first search

---
+++ src/Graphs/Graph_Implementation/BFS.java
@@ -0,0 +1,72 @@
+package Graphs.Graph_Implementation;
+
+import java.util.LinkedList;
+
+
+public class BFS {
+
+    private int numVertex;
+    private LinkedList<Integer>[] adjList;
+
+    public BFS(int numVertex) {
+
+        this.numVertex = numVertex;
+        adjList = new LinkedList[numVertex];
+
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+
+    }
+
+
+    private void addEdge(int u, int v) {
+        adjList[u].add(v);
+    }
+
+
+    private void traversal(int source) {
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[numVertex];
+        queue.add(source);
+        visited[source] = true;
+
+
+        while (queue.size() != 0) {
+
+            int vertex = queue.poll();
+            LinkedList<Integer> adjVertex = adjList[vertex];
+            System.out.print(vertex + " ");
+
+
+            for (int i = 0; i < adjVertex.size(); i++) {
+
+                if (!visited[adjVertex.get(i)]) {
+                    visited[adjVertex.get(i)] = true;
+                    queue.add(adjVertex.get(i));
+                }
+
+            }
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        BFS bfs = new BFS(4);
+        bfs.addEdge(0, 1);
+        bfs.addEdge(0, 2);
+        bfs.addEdge(1, 2);
+        bfs.addEdge(2, 0);
+        bfs.addEdge(2, 3);
+        bfs.addEdge(3, 3);
+
+        bfs.traversal(2);
+
+    }
+
+
+}
