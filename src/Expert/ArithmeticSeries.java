cc93c6d3b1b904d7af2018dd487b5d6e42bb2816
PrajwalM2212 <prajwalmmath@gmail.com>
2018-08-30 20:55:53 +0530

    dp

--- src/Expert/ArithmeticSeries.java
+++ src/Expert/ArithmeticSeries.java
@@ -4,14 +4,15 @@ public class ArithmeticSeries {
 
     private int numRecur = 0;
 
-    private int solve(int[] arr) {
+    private int solve(int[] arr, int[][] cache) {
 
         int num = 0;
         for (int i = 0; i < arr.length; i++) {
 
             for (int j = i + 1; j < arr.length; j++) {
 
-                num += solveRecur(arr, i, arr[j] - arr[i], 1);
+                num += solveRecur(arr, i, arr[j] - arr[i], 1, cache);
+                // System.out.println(i + " " + (arr[j] - arr[i]) + " " + cache[i][arr[j] - arr[i]]);
 
             }
 
@@ -20,7 +21,11 @@ public class ArithmeticSeries {
 
     }
 
-    private int solveRecur(int[] arr, int i, int diff, int count) {
+    private int solveRecur(int[] arr, int i, int diff, int count, int[][] cache) {
+
+        if (count > 2 && cache[i][diff] != 0) {
+            return cache[i][diff];
+        }
 
         numRecur++;
 
@@ -31,10 +36,10 @@ public class ArithmeticSeries {
             if (arr[ind] - arr[i] == diff) {
 
                 if (count > 2) {
-                    times = 1 + solveRecur(arr, ind, diff, count + 1);
+                    times = 1 + solveRecur(arr, ind, diff, count + 1, cache);
                     isMove = true;
                 } else {
-                    times += solveRecur(arr, ind, diff, count + 1);
+                    times += solveRecur(arr, ind, diff, count + 1, cache);
                     isMove = true;
                 }
 
@@ -44,18 +49,23 @@ public class ArithmeticSeries {
         }
 
         if (!isMove && count > 2) {
-            System.out.println("I have reached " + arr[i] + " with a difference " + diff + " and contain " + count +" elements");
-            return 1;
+            // System.out.println("I have reached " + arr[i] + " with a difference " + diff + " and contain " + count + " elements");
+            return cache[i][diff] = 1;
+        }
+
+        if (count > 2) {
+            cache[i][diff] = times;
         }
 
         return times;
+
 
     }
 
 
     public static void main(String[] args) {
         ArithmeticSeries arithmeticSeries = new ArithmeticSeries();
-        System.out.println(arithmeticSeries.solve(new int[]{2,3,6,4,8,9,10}));
+        System.out.println(arithmeticSeries.solve(new int[]{1, 1, 1, 2, 3, 4, 5}, new int[9][30]));
         System.out.println("Number of recursive calls " + arithmeticSeries.numRecur);
     }
 }
